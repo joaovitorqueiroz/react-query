774a8f4fa41a2c4b4c965185c7056a202fa79623
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2023-05-17 00:04:28 -0300

    chore: install react query

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import {Routes} from './src/routes';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <Routes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
