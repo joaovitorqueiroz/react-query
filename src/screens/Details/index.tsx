002da2427f519c5e044bcc74181a9119df56ed3f
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2023-05-17 01:49:02 -0300

    chore: refactor details with mutation on save

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,17 +1,18 @@
 import React, {useState} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import {UserDetails, ModalEditUser} from '../../components';
-import {EditUser, editUser} from '../../services/api/user';
+import {EditUser} from '../../services/api/user';
 import {useRoute} from '@react-navigation/native';
 import {DetailsParams} from '../../@types/navigation';
 import styles from './styles';
-import {useGetUserDetails} from '../../queries/user';
+import {useEditUser, useGetUserDetails} from '../../queries/user';
 
 const Details = () => {
   const [showModal, setShowModal] = useState(false);
   const route = useRoute();
   const {id} = route.params as DetailsParams;
   const {isLoading, isError, data} = useGetUserDetails(id);
+  const {mutate: editUser} = useEditUser();
 
   const onSave = (user: EditUser) => {
     editUser(user);
